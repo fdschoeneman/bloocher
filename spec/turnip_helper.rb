32f44764df9b03f0b96ee780f9914f23b13787ab
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-01-07 17:31:16 -0800

    added Guardfile, spec helper and turnip helper

---
+++ spec/turnip_helper.rb
@@ -0,0 +1,9 @@
+Dir[Rails.root.join("spec/acceptance/steps/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/acceptance/steps*.rb")].each {|f| require f}
+Dir[Rails.root.join("lib/turnip/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.include AuthenticationSteps
+  config.include EmailHandlingSteps
+  config.include UtilitySteps
+end
