6a25520076322535ef018dafc613e59a5a5e3e1b
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-01-07 16:08:09 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloocher::Application.config.secret_token = '4b59cb9e4e4c94e2e601380379dba2d3ec252313b1532fbccdb7b128ff50593ccfb1863e75c098b14a2c47e507cd25ff139b5d04884c9b8fb225c2f062bdf3a5'
