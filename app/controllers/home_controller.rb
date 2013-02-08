d08546cac3d6d4f9debe7bd63cd46e0b19b23469
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-02-08 02:12:11 -0800

    removed triq.css, whic hfixed some ugly
    gi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @page_title = "home"
+    @user = User.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,16 @@ module ApplicationHelper
       link_to text, path
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
