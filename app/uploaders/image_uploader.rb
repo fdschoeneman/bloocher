f3ddb824ddf6b018dd9d4df89ac65d035934d69e
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-08-03 01:06:34 -0700

    removed extra app yaml, aded carrierwave mimethpes module, added set content type method to processing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   retina!  
 
+  include CarrierWave::MimeTypes
+
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -15,6 +18,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   storage :fog
+
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
