eac774a946420646bb7b994a0d548410cd660507
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-09-30 20:07:41 -0700

    got rid of certifications pro model

--- app/models/certifications_producer.rb
+++
@@ -1,5 +0,0 @@
-class CertificationsProducer < ActiveRecord::Base
-
-  belongs_to :producer
-  belongs_to :certification
-end
--- test/factories/certifications_producers.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :certifications_producer do
-    # producer_id 1
-    # certification_id 1
-    certification
-    producer
-  end
-end
