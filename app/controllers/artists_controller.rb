427664f02f8cdb24d9733ef3b0f1c148f85b0362
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2013-11-09 15:30:57 -0800

    cleaned up artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,25 +1,20 @@
 class ArtistsController < ApplicationController
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
-  # GET /artists
   def index
     @artists = Artist.all
   end
 
-  # GET /artists/1
   def show
   end
 
-  # GET /artists/new
   def new
     @artist = Artist.new
   end
 
-  # GET /artists/1/edit
   def edit
   end
 
-  # POST /artists
   def create
     @artist = Artist.new(artist_params)
 
@@ -30,7 +25,6 @@ class ArtistsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /artists/1
   def update
     if @artist.update(artist_params)
       redirect_to @artist, notice: 'Artist was successfully updated.'
@@ -39,20 +33,17 @@ class ArtistsController < ApplicationController
     end
   end
 
-  # DELETE /artists/1
   def destroy
     @artist.destroy
     redirect_to artists_url, notice: 'Artist was successfully destroyed.'
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_artist
-      @artist = Artist.find(params[:id])
-    end
+private
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def artist_params
-      params.require(:artist).permit(:artist_id, :statement)
-    end
+  def artist_params
+    params.require(:artist).permit(:artist_id, :statement)
+  end
 end
